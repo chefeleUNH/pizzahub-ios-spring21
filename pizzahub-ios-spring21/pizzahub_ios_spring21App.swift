7cecc14beb7c4a2a9c9e649bf664c03ffa0c17e6
chefeleUNH <chefele@newhaven.edu>
2021-03-16 20:15:53 -0400

    Initial Commit

---
+++ pizzahub-ios-spring21/pizzahub_ios_spring21App.swift
@@ -0,0 +1,17 @@
+//
+//  pizzahub_ios_spring21App.swift
+//  pizzahub-ios-spring21
+//
+//  Created by Charles Hefele on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct pizzahub_ios_spring21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
